224d123de3813e840ab5a63ecb4d7136e4669583
SEALBEAN <xuhet2017@gmail.com>
2022-10-15 11:14:12 +0700

    Add Ticket GET API

--- src/main/java/com/example/promechbackend/controller/TicketController.java
+++ src/main/java/com/example/promechbackend/controller/TicketController.java
@@ -1,10 +1,27 @@
 package com.example.promechbackend.controller;
 
+import com.example.promechbackend.entity.TicketEntity;
+import com.example.promechbackend.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("Ticket")
 public class TicketController {
+    @Autowired
+    TicketService ticketService;
+    @GetMapping("/{phone}")
+    TicketEntity getTicketByPhone(@PathVariable int phone){
+        return ticketService.getTicketByPhone(phone);
+    }
 
+    @GetMapping("/ALL")
+    List<TicketEntity> getTicketByPhone(){
+        return ticketService.getAll();
+    }
 }
--- src/main/java/com/example/promechbackend/service/TicketService.java
+++ src/main/java/com/example/promechbackend/service/TicketService.java
@@ -1,11 +1,22 @@
 package com.example.promechbackend.service;
 
+import com.example.promechbackend.entity.TicketEntity;
 import com.example.promechbackend.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TicketService {
     @Autowired
     TicketRepository ticketRepository;
+
+    public TicketEntity getTicketByPhone(int phone) {
+        return ticketRepository.findById(phone).get();
+    }
+
+    public List<TicketEntity> getAll() {
+        return ticketRepository.findAll();
+    }
 }
